51c035516985ebfa16cb8f19df2848ddc402e79c
Hawkman110 <haukurodins@gmail.com>
2017-03-31 23:07:13 +0000

    búinn að tengja allar sign up síðurnar við gagnagrun

--- input2.php
+++ input2.php
@@ -3,8 +3,8 @@ $servername = "10.200.10.24";
 $username = "1801993229";
 $password = "Thehawk110";
 $dbname = "1801993229_mutant";
-$name = $_POST['name'];
-$mutantName = $_POST['mutantName'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
 $email = $_POST['email'];
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,11 +13,11 @@ if ($conn->connect_error) {
   die("connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO signup (name, mutantName, email)
-VALUES (\"$name\", \"$mutantName\", \"$email\" )";
+$sql = "INSERT INTO signupfights (firstName, lastName, email)
+VALUES (\"$firstName\", \"$lastName\", \"$email\" )";
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+  echo "sign up successfull";
   echo "<a href=\"http://tsuts.tskoli.is/2t/1801993229/vshSkilaverk4/dis%20folder%20hars%20the%20real%20git%204/index.html\">Fara til baka hérna</a>";
 
   } else {
--- input3.php
+++ input3.php
@@ -3,8 +3,8 @@ $servername = "10.200.10.24";
 $username = "1801993229";
 $password = "Thehawk110";
 $dbname = "1801993229_mutant";
-$name = $_POST['name'];
-$mutantName = $_POST['mutantName'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
 $email = $_POST['email'];
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,12 +13,12 @@ if ($conn->connect_error) {
   die("connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO signup (name, mutantName, email)
-VALUES (\"$name\", \"$mutantName\", \"$email\" )";
+$sql = "INSERT INTO signupfood (firstName, lastName, email)
+VALUES (\"$firstName\", \"$lastName\", \"$email\" )";
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-  echo "<a href=\"http://tsuts.tskoli.is/2t/1801993229/vshSkilaverk4/dis%20folder%20hars%20the%20real%20git%204/index.html\">Fara til baka hérna</a>";
+  echo "sign up successfull";
+  echo "<a href=\"http://tsuts.tskoli.is/2t/1801993229/vshSkilaverk4/dis%20folder%20hars%20the%20real%20git%204/mutantFood.html\">Fara til baka hérna</a>";
 
   } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
